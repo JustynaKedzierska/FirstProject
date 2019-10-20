332bcb54a23d824b6dabbc9e4450f5711b70d8de
Justyna Kędzierska <jus.kedzierska@gmail.com>
2019-10-20 13:05:31 +0200

    Added views: User (create, list), Language (create,list, update, delete), Question (create, list, update, delete), QuestionList (create, list)

--- FirstProject/urls.py
+++ FirstProject/urls.py
@@ -1,4 +1,4 @@
-"""FirstProject URL Configuration
+"""HRquestion URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -12,10 +12,28 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# """
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from HRQuestions.views import UserCreateView, QuestionCreateView, LanguageCreateView, LanguageListView, \
+    LanguageUpdateView, QuestionListView, QuestionUpdateView, LanguageDeleteView, QuestionListCreateView, UserListView, \
+    QuestionListListView, QuestionDeleteView, IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='index'),
+    url(r'^user/add', UserCreateView.as_view(), name='user-add'),
+    url(r'^user/list', UserListView.as_view(), name='user-list'),
+    url(r'^question/add', QuestionCreateView.as_view(), name='question-add'),
+    url(r'^question/list', QuestionListView.as_view(), name='question-list'),
+    url(r'^question/update/(?P<pk>(\d)+)', QuestionUpdateView.as_view(), name='question-update'),
+    url(r'^question/delete/(?P<pk>(\d)+)', QuestionDeleteView.as_view(), name='question-delete'),
+    url(r'^language/add', LanguageCreateView.as_view(), name='language-add'),
+    url(r'^language/list', LanguageListView.as_view(), name='language-list'),
+    url(r'^language/update/(?P<pk>(\d)+)', LanguageUpdateView.as_view(), name='language-update'),
+    url(r'^language/delete/(?P<pk>(\d)+)', LanguageDeleteView.as_view(), name='language-delete'),
+    url(r'^question_group/add', QuestionListCreateView.as_view(), name='question-group-add'),
+    url(r'^question_group/list', QuestionListListView.as_view(), name='question-group-list'),
 ]
--- HRQuestions/views.py
+++ HRQuestions/views.py
@@ -1,3 +1,84 @@
+from django.contrib.auth.models import User, Group
 from django.shortcuts import render
 
 # Create your views here.
+from django.template.response import TemplateResponse
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import FormView, CreateView, ListView, UpdateView, DeleteView
+
+from HRQuestions.models import Question, Language, QuestionList
+
+
+class IndexView(View):
+    pass
+
+
+class UserCreateView(CreateView):
+    template_name = 'AUTH/add_user_form.html'
+    model = User
+    fields = ['username', 'first_name', 'last_name', 'email', 'password']
+    success_url = reverse_lazy('index')
+
+
+class UserListView(ListView):
+    model = User
+    paginate_by = 10
+
+
+class QuestionCreateView(CreateView):
+    template_name = 'question_create.html'
+    model = Question
+    fields = ['title', 'author', 'language', 'question_type', 'level', 'content']
+    success_url = reverse_lazy('question-list')
+
+
+class QuestionListView(ListView):
+    model = Question
+    paginate_by = 10
+
+
+class QuestionUpdateView(UpdateView):
+    model = Question
+    fields = ['title', 'question_type', 'level', 'language', 'content']
+    template_name_suffix = '_update_form'
+
+
+class QuestionDeleteView(DeleteView):
+    model = Question
+    success_url = reverse_lazy('question-list')
+
+
+class LanguageCreateView(CreateView):
+    template_name = 'language_create.html'
+    model = Language
+    fields = ['name']
+    success_url = reverse_lazy('language-list')
+
+
+class LanguageListView(ListView):
+    model = Language
+    paginate_by = 10
+
+
+class LanguageUpdateView(UpdateView):
+    model = Language
+    fields = ['name']
+    template_name_suffix = '_update_form'
+
+
+class LanguageDeleteView(DeleteView):
+    model = Language
+    success_url = reverse_lazy('language-list')
+
+
+class QuestionListCreateView(CreateView):
+    template_name = 'question_list_create.html'
+    model = QuestionList
+    fields = ['name', 'author', 'question']
+    success_url = reverse_lazy('question-list')
+
+
+class QuestionListListView(ListView):
+    model = QuestionList
+    paginate_by = 10
